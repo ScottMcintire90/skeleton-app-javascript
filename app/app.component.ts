c7e3d2dd915e3d203d5d59a429c1e3f61a406c4a
Scott <scottmcintire90@gmail.com>
2016-05-31 15:40:41 -0700

    initial commit, displays tasks

---
+++ app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>To-Do List</h1>
+      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
+        {{ task.description }}
+      </h3>
+    </div>
+  `
+})
+
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  template: `
+  <div class="container">
+    <h1>To-Do List</h1>
+    <task-list [taskList]="tasks"></task-list>
+  </div>
+`
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  taskClicked(clickedTask: Task): void {
+    console.log(clickedTask);
+  }
+}
+
+export class AppComponent {
+  public tasks: Task[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask);
+  }
+}
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
+}
