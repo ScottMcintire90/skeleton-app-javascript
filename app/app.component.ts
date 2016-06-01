c0f259e706e53d970821f88c17a2e7c7a4166aa1
Scott <scottmcintire90@gmail.com>
2016-05-31 18:53:08 -0700

    works correctly

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,51 +1,13 @@
 import { Component, EventEmitter } from 'angular2/core';
-
-@Component({
-    selector: 'task-display',
-    inputs: ['task'],
-  template: `
-    <h3>{{ task.description }}</h3>
-  `
-})
-
-export class TaskComponent {
-  public task: Task;
-}
-
-@Component({
-  selector: 'task-list',
-  inputs: ['taskList'],
-  outputs: ['onTaskSelect'],
-  directives: [TaskComponent],
-  template: `
-  <task-display *ngFor="#currentTask of taskList"
-    (click)="taskClicked(currentTask)"
-    [class.selected]="currentTask === selectedTask"
-    [task]="currentTask">
-  </task-display>
-  `
-})
-export class TaskListComponent {
-  public taskList: Task[];
-  public onTaskSelect: EventEmitter<Task>;
-  public selectedTask: Task;
-  constructor() {
-    this.onTaskSelect = new EventEmitter();
-  }
-  taskClicked(clickedTask: Task): void {
-    console.log('child', clickedTask);
-    this.selectedTask = clickedTask;
-    this.onTaskSelect.emit(clickedTask);
-  }
-}
-
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
   directives: [TaskListComponent],
   template: `
     <div class="container">
-      <h1>To-Do List</h1>
+      <h1>To-Do List!</h1>
       <task-list
         [taskList]="tasks"
         (onTaskSelect)="taskWasSelected($event)">
@@ -53,8 +15,9 @@ export class TaskListComponent {
     </div>
   `
 })
+
 export class AppComponent {
-  public tasks: Task[];
+  public tasks: Task[]; //Task[] (or Array<Task>) identifies tasks as an array of Task objects
   constructor(){
     this.tasks = [
       new Task("Create To-Do List app.", 0),
@@ -65,12 +28,5 @@ export class AppComponent {
   }
   taskWasSelected(clickedTask: Task): void {
     console.log('parent', clickedTask);
-  }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {
-
   }
 }
